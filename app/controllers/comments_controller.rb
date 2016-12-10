16c39b90f51c220074ea24eb73ff01cd6044beb7
duck105 <jacky87810@gmail.com>
2016-12-10 17:18:54 +0800

    Update edit commit function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,8 @@ class CommentsController < ApplicationController
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.find(params[:id])
 
-		@comment.update
+		@comment.update(comment_params)
+		redirect_to post_path(@post)
 	end
 
 	private
