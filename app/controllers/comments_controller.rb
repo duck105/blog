60a71c0b1520d7d14b5fff4615ac0de0bb3aa977
duck105 <jacky87810@gmail.com>
2017-02-17 18:49:50 +0800

    Make new comment more elegent and indentation great again

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,36 @@
 class CommentsController < ApplicationController
-	before_action :authenticate_user!  
-	def create
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(comment_params)
+  before_action :authenticate_user!
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
 
-		redirect_to post_path(@post)
-	end
+    redirect_to post_path(@post)
+  end
 
-	def destroy
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.find(params[:id])
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
 
-		@comment.destroy
+    @comment.destroy
 
-		redirect_to post_path(@post)
-	end
+    redirect_to post_path(@post)
+  end
 
-	def edit
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.find(params[:id])
-	end
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
 
-	def update
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.find(params[:id])
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
 
-		@comment.update(comment_params)
-		redirect_to post_path(@post)
-	end
+    @comment.update(comment_params)
+    redirect_to post_path(@post)
+  end
 
-	private
-		def comment_params
-			params.require(:comment).permit(:name, :body)
-		end
+  private
+  def comment_params
+    params.require(:comment).permit(:name, :body)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,57 +1,58 @@
 class PostsController < ApplicationController
-	before_action :authenticate_user!, except: [:index, :show]
-	def index
-		@posts = Post.page(params[:page]).per(5)
-	end
+  before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @posts = Post.page(params[:page]).per(5)
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		@post = current_user.posts.new(post_params)
-		if @post.save
-			redirect_to post_path(@post)
-			flash[:notice] = "post was successfully created"
-		else 
-			render 'new'
-		end
-	end
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+      flash[:notice] = "post was successfully created"
+    else
+      render 'new'
+    end
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
 
-	def edit
-		@post = current_user.posts.find(params[:id])
-	end
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
 
-	def update
-		@post = current_user.posts.find(params[:id])
+  def update
+    @post = current_user.posts.find(params[:id])
 
-		if @post.update(post_params)
-			redirect_to post_path(@post)
-			flash[:notice] = "post was successfully updated"
-		else 
-			render 'edit'
-		end
-	end
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+      flash[:notice] = "post was successfully updated"
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@post = current_user.posts.find(params[:id])
-		@post.destroy
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
 
-		redirect_to root_path
-		flash[:alert] = "post was successfully destroyed"
-	end
+    redirect_to root_path
+    flash[:alert] = "post was successfully destroyed"
+  end
 
-	def who
-		
-	end
+  def who
+
+  end
 
 
-	private
-		def post_params
-			params.require(:post).permit(:image, :title, :body, :user_id)
-		end
+  private
+  def post_params
+    params.require(:post).permit(:image, :title, :body, :user_id)
+  end
 end
